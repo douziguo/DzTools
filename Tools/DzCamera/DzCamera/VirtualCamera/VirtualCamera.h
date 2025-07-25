2f542151c9b16cee2ed506efe3f7420d70079b8c
douziguo <506966746@qq.com>
2025-07-25 15:50:50 +0800

    update virtual

--- Tools/DzCamera/DzCamera/VirtualCamera/VirtualCamera.h
+++ Tools/DzCamera/DzCamera/VirtualCamera/VirtualCamera.h
@@ -1,46 +1,1 @@
-﻿#ifndef VirtualCamera_H
-#define VirtualCamera_H
-
-#include "../Public/DzCamera.h"
-
-namespace DzCamera {
-
-class VirtualCamera : public Camera
-{
-    Q_OBJECT
-private:
-    QString _deviceSerial;
-
-    bool _IOEnable = false;
-    int _packetSize = 500;
-    qreal _expTime = 0;
-    qreal _digitalShift = 0;
-    qreal _gain = 0;
-    TriggerMode _triggerMode = SoftwareTriggering;
-public:
-    VirtualCamera(QObject *parent = nullptr);
-
-    QStringList enumerateDevicesSerial() override;
-
-    bool attachBySerial(const QString &serial) override;
-    bool detach() override;
-
-    QImage grab() override;
-
-    QString serialNumber() override;
-
-    bool setIOEnable(const bool &isEnable) override;
-    bool setPacketSize(const int &_packetSize) override;
-    bool setExposureTime(const qreal &_expTime) override;
-    bool setDigitalShift(const qreal digitalShift) override;
-    bool setGain(const qreal gain) override;
-    bool setTriggerMode(TriggerMode triggerMode) override;
-private:
-    bool checkNodeMap();
-    QColor getRandColor();
-    void delay(int msec);
-};
-
-}   // namespace DzCamera
-
-#endif // VirtualCamera_H
+#ifndef VirtualCamera_H#define VirtualCamera_H#include "../Public/DzCamera.h"namespace DzCamera {class VirtualCamera : public Camera{    Q_OBJECTprivate:    QString _deviceSerial;    bool _IOEnable = false;    int _packetSize = 500;    qreal _expTime = 0;    qreal _digitalShift = 0;    qreal _gain = 0;    TriggerMode _triggerMode = SoftwareTriggering;public:    VirtualCamera(QObject *parent = nullptr);    QStringList enumerateDevicesSerial() override;    bool attachBySerial(const QString &serial) override;    bool detach() override;    QImage grab() override;    QString serialNumber() override;    bool setIOEnable(const bool &isEnable) override;    bool setPacketSize(const int &_packetSize) override;    bool setExposureTime(const qreal &_expTime) override;    bool setDigitalShift(const qreal digitalShift) override;    bool setGain(const qreal gain) override;    bool setTriggerMode(TriggerMode triggerMode) override;private:    bool checkNodeMap();    QColor getRandColor();    void delay(int msec);};}   // namespace DzCamera#endif // VirtualCamera_H
